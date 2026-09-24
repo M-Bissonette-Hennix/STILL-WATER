0d9c9a4948daf49de1d2fee8bdc01b9249d6c2e6
M. Bissonette Hennix <m.bissonette.hennix@proton.me>
2026-09-24 13:22:14 -0400

    Add files via upload

--- src/app/protocol-ui.js
+++ src/app/protocol-ui.js
@@ -1,13 +1,13 @@
 export const KUJI = Object.freeze([
-  { state: 'KUJI_RIN', ordinal: 1, name: 'RIN', kanji: '臨', mudra: 'Dokko-in' },
-  { state: 'KUJI_PYO', ordinal: 2, name: 'PYŌ', kanji: '兵', mudra: 'Daikongō-in' },
-  { state: 'KUJI_TO', ordinal: 3, name: 'TŌ', kanji: '闘', mudra: 'Sotojishi-in' },
-  { state: 'KUJI_SHA', ordinal: 4, name: 'SHA', kanji: '者', mudra: 'Uchijishi-in' },
-  { state: 'KUJI_KAI', ordinal: 5, name: 'KAI', kanji: '皆', mudra: 'Gebakuken-in' },
-  { state: 'KUJI_JIN', ordinal: 6, name: 'JIN', kanji: '陣', mudra: 'Naibakuken-in' },
-  { state: 'KUJI_RETSU', ordinal: 7, name: 'RETSU', kanji: '列', mudra: 'Chiken-in' },
-  { state: 'KUJI_ZAI', ordinal: 8, name: 'ZAI', kanji: '在', mudra: 'Nichirin-in' },
-  { state: 'KUJI_ZEN', ordinal: 9, name: 'ZEN', kanji: '前', mudra: 'Ongyō-in / lineage-specific Yin mudra' }
+  { state: 'KUJI_RIN', ordinal: 1, name: 'RIN', kanji: '臨', mudra: 'Dokko-in', image: './assets/kuji/01-rin.jpg' },
+  { state: 'KUJI_PYO', ordinal: 2, name: 'PYŌ', kanji: '兵', mudra: 'Daikongō-in', image: './assets/kuji/02-pyo.jpg' },
+  { state: 'KUJI_TO', ordinal: 3, name: 'TŌ', kanji: '闘', mudra: 'Sotojishi-in', image: './assets/kuji/03-to.jpg' },
+  { state: 'KUJI_SHA', ordinal: 4, name: 'SHA', kanji: '者', mudra: 'Uchijishi-in', image: './assets/kuji/04-sha.jpg' },
+  { state: 'KUJI_KAI', ordinal: 5, name: 'KAI', kanji: '皆', mudra: 'Gebakuken-in', image: './assets/kuji/05-kai.jpg' },
+  { state: 'KUJI_JIN', ordinal: 6, name: 'JIN', kanji: '陣', mudra: 'Naibakuken-in', image: './assets/kuji/06-jin.jpg' },
+  { state: 'KUJI_RETSU', ordinal: 7, name: 'RETSU', kanji: '列', mudra: 'Chiken-in', image: './assets/kuji/07-retsu.jpg' },
+  { state: 'KUJI_ZAI', ordinal: 8, name: 'ZAI', kanji: '在', mudra: 'Nichirin-in', image: './assets/kuji/08-zai.jpg' },
+  { state: 'KUJI_ZEN', ordinal: 9, name: 'ZEN', kanji: '前', mudra: 'Ongyō-in / lineage-specific Yin mudra', image: './assets/kuji/09-zen.jpg' }
 ]);
 
 export const TRAIN_TIMED_STATES = Object.freeze({
